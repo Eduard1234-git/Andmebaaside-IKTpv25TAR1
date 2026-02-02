043cb5ef5f9678cddb1c0eba9516f1cc86510ee7
Eduard1234-git <kotkas.eduard@gmail.com>
2026-02-02 16:07:19 +0200

    Modify opilane.sql with various SQL commands
    Added SQL commands for selecting, dropping, inserting data, and altering the 'opilane' table.

--- opilane.sql
+++ opilane.sql
@@ -8,4 +8,18 @@ perenimi varchar(30) NOT null,
 synniaeg date,
 aadress TEXT, 
 kas_opib bit );
+--kuva tabeli, *-kõik väljad
 SELECT * FROM opilane;
+
+--tabeli kustutamine
+DROP TABLE opilane;
+
+--andmete lisamine tableisse opilane
+INSERT INTO opilane(eesnimi, perenimi, synniaeg, kas_opib)
+VALUES ('Nikita', 'Petrov', '2023-12-12', 0),
+('Nikita', 'Alekseev', '2019-11-12', 0),
+('Nikita', 'NIlitiov', '2021-12-13', 10);
+Select * from opilane;
+
+--muudame tabeli ja lisame piirangu UNIQUE
+ALTER TABLE oplinae Alter column perenimi varchar (30) UNIQUE;
