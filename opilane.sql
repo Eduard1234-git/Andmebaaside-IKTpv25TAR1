655385d734081cfda5945abf98ffdc8dda216086
Eduard1234-git <kotkas.eduard@gmail.com>
2026-02-02 15:26:52 +0200

    Create database and table for students

---
+++ opilane.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE kotkasSQL; 
+use kotkasSQL;
+--Ttabeli loomine
+CREATE TABLE opilane(
+opilaneID int PRIMARY KEY identity(1,1),
+eesnimi varchar(25),
+perenimi varchar(30) NOT null,
+synniaeg date,
+aadress TEXT, 
+kas_opib bit );
+SELECT * FROM opilane;
